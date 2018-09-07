2d6f731eee7ed87eb53255ecf518a658ff6294ed
andyxiong <627020231@qq.com>
2018-09-07 10:51:29 +0800

    change config of firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
 
 export default {
-    apiKey: "AIzaSyCCt7vK9T8WjS7ZRcyyld4HbGjGMXJEGXQ",
-    authDomain: "gfresh-srm.firebaseapp.com",
-    databaseURL: "https://gfresh-srm.firebaseio.com",
-    projectId: "gfresh-srm",
-    storageBucket: "gfresh-srm.appspot.com",
-    messagingSenderId: "826950657205"
+    apiKey: "AIzaSyC37ztj8pk502V4s3DGswPTcKGELfHVHx0",
+    authDomain: "lobster-quote.firebaseapp.com",
+    databaseURL: "https://lobster-quote.firebaseio.com",
+    projectId: "lobster-quote",
+    storageBucket: "lobster-quote.appspot.com",
+    messagingSenderId: "912655517052"
 };
